6e2c59b9d26015ed4fc54390e02438b108538404
saigsf <2270029397@qq.com>
2017-11-23 15:00:00 +0800

    背景分裂

---
+++ src/js/calendar.js
@@ -0,0 +1,47 @@
+window.onload = function() {
+    window.initdefault();
+    var d = new Division({
+        el: document.getElementsByClassName("container")[0],
+        rows: 7,
+        cols: 7,
+        isEvents: false,
+        isEventsSub: true
+    });
+    window.onresize = function() {
+        window.initdefault();
+        d.init();
+    }
+
+    console.log(d)
+
+    // var reset = document.getElementsByClassName("reset")[0];
+    var change = document.getElementsByClassName("change")[0];
+    // var url = document.getElementsByClassName("url")[0];
+    // var ok = document.getElementsByClassName("ok")[0];
+    var img = document.getElementsByTagName("img")[0];
+    // console.log(reset, change, url, ok, img)
+
+
+    var imgURL = ['./src/img/1490073118356.jpg',
+        './src/img/1490338321679.jpg',
+        'http://img05.tooopen.com/images/20140404/sy_58241958989.jpg',
+        'http://pic4.nipic.com/20091217/3885730_124701000519_2.jpg'
+    ];
+    var index = 0;
+    change.onclick = function() {
+        index++;
+        if (index >= imgURL.length) {
+            index = 0;
+        }
+        d.changeBgimg(imgURL[index]);
+        img.src = imgURL[index];
+
+    }
+
+    // ok.onclick = function() {
+    //     var urlText = url.value;
+    //     d.changeBgimg(urlText);
+    //     img.src = urlText;
+    // }
+
+}
--- src/js/unit.js
+++ src/js/unit.js
@@ -41,6 +41,8 @@
 /**
  * 数组的相关操作
  * 兼容处理，判断是否支持indexof，如果不支持则添加indexof方法
+ * 作者：Saber
+ * 时间：2017/11/23
  */
 (function() {
     if (!Array.prototype.indexOf) {
@@ -102,7 +104,7 @@
         }
     };
     var str = "123789045789067890";
-    console.log(str.strOperations(2, 3).strUnique());
+    // console.log(str.strOperations(2, 3).strUnique());
 })();
 
 
@@ -117,9 +119,9 @@
     var htmlBox = document.documentElement;
     var WIDTH_HEIGHT_RATE = parseInt(9 / 16 * 1000) / 1000;
     var MAX_WIDTH = 1920; //以1920*1080为标准
-    var MAX_FONT_SIZE = 100; // 与MAX_WIDTH对应的HTML的font-size
-
-    var init = function() {
+    var MAX_FONT_SIZE = 64; // 与MAX_WIDTH对应的HTML的font-size
+    // 大屏展示
+    var initbig = function() {
         var viewport = window.getInfo();
         var ratio = parseInt(viewport.winH / viewport.winW * 1000) / 1000;
 
@@ -137,5 +139,170 @@
             htmlBox.style.height = viewport.winH + "px";
         }
     }
-    window.initsize = init;
+    var initdefault = function() {
+        var viewport = window.getInfo();
+        var ratio = parseInt(viewport.winH / viewport.winW * 1000) / 1000;
+
+        if (ratio > WIDTH_HEIGHT_RATE) {
+            htmlBox.style.fontSize = viewport.winW * MAX_FONT_SIZE / MAX_WIDTH + "px";
+            htmlBox.style.width = viewport.winW + "px";
+        } else {
+            htmlBox.style.fontSize = viewport.winW * MAX_FONT_SIZE / MAX_WIDTH + "px";
+            htmlBox.style.width = '100 %';
+        }
+    }
+    window.initsize = initbig;
+    window.initdefault = initdefault;
+})();
+
+/**
+ * 计算后样式
+ * 作者：Saber
+ * 时间：2017/11/23
+ */
+(function() {
+    var fetchComputedStyle = function(obj, property) {
+        //能力检测
+        if (window.getComputedStyle) {
+            //现在要把用户输入的property中检测一下是不是驼峰，转为连字符写法
+            //强制把用户输入的词儿里面的大写字母，变为小写字母加-
+            //paddingLeft  →  padding-left
+            property = property.replace(/([A-Z])/g, function(match, $1) {
+                return "-" + $1.toLowerCase();
+            });
+
+            return window.getComputedStyle(obj)[property];
+        } else {
+            //IE只认识驼峰，我们要防止用户输入短横，要把短横改为大写字母
+            //padding-left  → paddingLeft 
+            property = property.replace(/\-([a-z])/g, function(match, $1) {
+                return $1.toUpperCase();
+            });
+
+            return obj.currentStyle[property];
+        }
+    }
+    window.fetchComputedStyle = fetchComputedStyle;
+})();
+
+/**
+ * 背景分裂效果
+ * 作者：Saber
+ * 时间：2017/11/23
+ */
+(function() {
+    var Division = function(opt) {
+        this.el = opt.el; //盒子元素
+        this.rows = opt.rows > 1 ? opt.rows : 4; // 行数
+        this.cols = opt.rows > 1 ? opt.rows : 4; // 列数
+        this.isEvents = opt.isEvents ? 1 : 0;
+        this.isEventsSub = opt.isEventsSub ? 1 : 0;
+        this.totalNum = opt.rows * opt.cols;
+        this.totalWidth = parseFloat(window.fetchComputedStyle(this.el, 'width')) - this.hasBorder(this.el) * 2; // 盒子的宽度
+        this.totalHeight = parseFloat(window.fetchComputedStyle(this.el, 'height')) - this.hasBorder(this.el) * 2; // 盒子的高度
+        this.subWidth = this.totalWidth / this.cols; // 每一小块儿的宽度
+        this.subHeight = this.totalHeight / this.rows; // 每一小块儿的高度
+        this.offsetsX = 0;
+        this.offsetsY = 0;
+        this.oDivs = [];
+        // console.log(this.hasBorder(this.el))
+
+        this.init();
+
+
+    }
+    Division.prototype.init = function() {
+        for (var i = 0; i < this.totalNum; i++) {
+            this.oDivs.push(document.createElement("div"));
+        }
+        this.render();
+        this.isEvents === 0 || this.events().enter();
+        this.isEvents === 0 || this.events().leave();
+        this.isEventsSub === 0 || this.subEvents().over();
+        this.isEventsSub === 0 || this.subEvents().out();
+    }
+    Division.prototype.render = function() {
+        /* 每次渲染都要重新计算 盒子的宽度和高度 */
+        this.totalWidth = parseFloat(window.fetchComputedStyle(this.el, 'width')); // 盒子的宽度
+        this.totalHeight = parseFloat(window.fetchComputedStyle(this.el, 'height')); // 盒子的高度
+        this.subWidth = this.totalWidth / this.cols; // 每一小块儿的宽度
+        this.subHeight = this.totalHeight / this.rows; // 每一小块儿的高度
+        for (var i = 0; i < this.totalNum; i++) {
+            // this.offsetsX = Math.floor(Math.random() * 200 - 100);
+            this.oDivs[i].style.width = this.subWidth + "px";
+            this.oDivs[i].style.height = this.subHeight + "px";
+            this.oDivs[i].style.top = this.offsetsX + "px";
+            this.oDivs[i].style.left = this.offsetsY + "px";
+            this.oDivs[i].style.backgroundSize = this.totalWidth + "px " + this.totalHeight + "px";
+            this.oDivs[i].style.backgroundPosition = -i % this.cols * this.subWidth + "px " + -Math.floor(i / this.rows) * this.subHeight + "px";
+
+            this.el.appendChild(this.oDivs[i]);
+        }
+    }
+    Division.prototype.events = function(callback) {
+        return {
+            enter: () => {
+                this.el.onmouseenter = () => {
+                    for (var i = 0; i < this.totalNum; i++) {
+                        this.offsetsX = Math.floor(Math.random() * 2 * this.subWidth - this.subWidth);
+                        this.offsetsY = Math.floor(Math.random() * 2 * this.subHeight - this.subHeight);
+                        this.oDivs[i].style.top = this.offsetsX + "px";
+                        this.oDivs[i].style.left = this.offsetsX + "px";
+                    }
+                }
+            },
+            leave: () => {
+                this.el.onmouseleave = () => {
+                    this.reset()
+                }
+            },
+            callback: callback
+        }
+    }
+
+
+    Division.prototype.subEvents = function() {
+        return {
+            over: () => {
+                for (var i = 0; i < this.totalNum; i++) {
+                    this.oDivs[i].onmouseover = function() {
+                        this.style.webkitBoxShadow = '0 0 0.4rem #066ef5';
+                        this.style.boxShadow = '0 0 0.4rem #066ef5';
+                        this.style.webkitTransform = 'translateZ(2rem)';
+                        this.style.transform = 'translateZ(2rem)';
+
+                    }
+
+                }
+            },
+            out: () => {
+                for (var i = 0; i < this.totalNum; i++) {
+                    this.oDivs[i].onmouseout = function() {
+                        this.style.webkitBoxShadow = 'none';
+                        this.style.boxShadow = 'none';
+                        this.style.webkitTransform = 'translateZ(0rem)';
+                        this.style.transform = 'translateZ(0rem)';
+                    }
+                }
+            }
+        }
+    }
+
+    Division.prototype.changeBgimg = function(url) {
+
+        for (var i = 0; i < this.totalNum; i++) {
+            this.oDivs[i].style.backgroundImage = 'url(' + url + ')'; // background-image: url("../img/1490073118356.jpg");
+        }
+    }
+    Division.prototype.hasBorder = function(el) {
+
+        return parseInt(window.fetchComputedStyle(el, 'borderWidth'))
+    }
+    Division.prototype.reset = function() {
+        for (var i = 0; i < this.totalNum; i++) {
+            this.oDivs[i].style.top = 0 + "px";
+            this.oDivs[i].style.left = 0 + "px";
+        }
+    }
+    window.Division = Division;
 })();
